a29cc7625836e48c1fc09e9af56aa2bf26561422
xsfour <xsfour@qq.com>
2020-03-21 19:38:57 +0800

    chore: remove built files from git repo

--- packages/canvas-player/lib/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var CanvasPlayer_1 = require("./CanvasPlayer");
-exports.CanvasPlayer = CanvasPlayer_1.CanvasPlayer;
-//# sourceMappingURL=index.js.map
