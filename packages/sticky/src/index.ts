ee6ed93cf78c7c67969859f19f51825268679e88
xsfour <xsfour@qq.com>
2020-04-03 19:06:32 +0800

    feat: can reset sticky item

--- packages/sticky/src/index.ts
+++ packages/sticky/src/index.ts
@@ -41,106 +41,149 @@ export function configure<T extends keyof Configs>(key: T, value: Configs[T]) {
   CONFIGS[key] = value;
 }
 
+function isStatic(position: string) {
+  return (
+    position !== 'relative' && position !== 'absolute' && position !== 'fixed'
+  );
+}
+
 export function initStickyElement(
   element: HTMLElement,
   { container: _container, scrollHandlers = {} }: StickyOption = {},
-): () => void {
-  let container: Element;
+) {
+  let container: HTMLElement;
   if (!_container) {
-    const parent = element.offsetParent;
+    const parent = element.parentElement;
     if (!parent) {
-      throw new Error(
-        'The container is not set explicitly; The element should have a NOT NULL offsetParent; See: https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/offsetParent',
-      );
+      throw new Error('Container not found');
     }
     container = parent;
   } else {
     container = _container;
   }
-  element.setAttribute(CONFIGS.addedFlagAttr, CONFIGS.addedFlagAttr);
-  element.style.position = 'relative';
 
-  const elementRect = element.getBoundingClientRect();
-  const containerRect = container.getBoundingClientRect();
-  if (CONFIGS.debug) {
-    // eslint-disable-next-line no-console
-    console.debug({
-      elementRect,
-      containerRect,
-      element,
-      container,
-    });
-  }
+  let removeHandle: ReturnType<typeof scrollHandle> | undefined;
+  const _init = () => {
+    const elementRect = element.getBoundingClientRect();
+    const containerRect = container.getBoundingClientRect();
+    if (CONFIGS.debug) {
+      // eslint-disable-next-line no-console
+      console.debug({
+        elementRect,
+        containerRect,
+        element,
+        container,
+      });
+    }
 
-  const bottomToContainerTop = elementRect.bottom - containerRect.top;
-  const top = `${elementRect.top - containerRect.top}px`;
-  const left = `${elementRect.left}px`;
-  const width = `${elementRect.width}px`;
-  const height = `${elementRect.height}px`;
+    const bottomToContainerTop = elementRect.bottom - containerRect.top;
+    const top = `${elementRect.top - containerRect.top}px`;
+    const left = `${elementRect.left}px`;
+    const width = `${elementRect.width}px`;
+    const height = `${elementRect.height}px`;
 
-  const removeHandle = scrollHandle(container, {
-    start: 'top',
-    end: { distance: bottomToContainerTop },
-    handlers: {
-      before: scrollHandlers.before,
-      inView: scrollHandlers.inView,
-      after: scrollHandlers.after,
-      onStateChange: (doms, state, oldState): void => {
-        switch (state) {
-          case 'inView':
-            setStyles(element, {
-              left,
-              top,
-              width,
-              height,
-              position: 'fixed',
-              bottom: 'auto',
-              transform: 'none',
-              margin: '0',
-            });
-            break;
-          case 'after':
-            setStyles(element, {
-              left: `${element.getBoundingClientRect().left -
-                container.getBoundingClientRect().left}px`,
-              top: 'auto',
-              bottom: '0',
-              position: 'absolute',
-              transform: 'none',
-              margin: '0',
-            });
-            break;
-          case 'before':
-          default:
-            setStyles(element);
-        }
-        if (CONFIGS.debug) {
-          const position =
-            state === 'inView' ? 'fixed' : state === 'after' ? 'absolute' : '';
-          // eslint-disable-next-line no-console
-          console.debug(`sticky item: ${position}`, element);
-        }
-        if (scrollHandlers.onStateChange) {
-          scrollHandlers.onStateChange(doms, state, oldState);
-        }
-      },
-    },
-  });
-  const destroy = (): void => {
     if (removeHandle) {
       removeHandle();
     }
+    removeHandle = scrollHandle(container, {
+      start: 'top',
+      end: { distance: bottomToContainerTop },
+      handlers: {
+        before: scrollHandlers.before,
+        inView: scrollHandlers.inView,
+        after: scrollHandlers.after,
+        onStateChange: (doms, state, oldState): void => {
+          switch (state) {
+            case 'inView':
+              setStyles(element, {
+                left,
+                top,
+                width,
+                height,
+                position: 'fixed',
+                bottom: 'auto',
+                transform: 'none',
+                margin: '0',
+              });
+              break;
+            case 'after':
+              setStyles(element, {
+                left: `${element.getBoundingClientRect().left -
+                  container.getBoundingClientRect().left}px`,
+                top: 'auto',
+                bottom: '0',
+                position: 'absolute',
+                transform: 'none',
+                margin: '0',
+              });
+              break;
+            case 'before':
+            default:
+              setStyles(element);
+          }
+          if (CONFIGS.debug) {
+            const position =
+              state === 'inView'
+                ? 'fixed'
+                : state === 'after'
+                ? 'absolute'
+                : '';
+            // eslint-disable-next-line no-console
+            console.debug(`sticky item: ${position}`, element);
+          }
+          if (scrollHandlers.onStateChange) {
+            scrollHandlers.onStateChange(doms, state, oldState);
+          }
+        },
+      },
+    });
   };
-  return destroy;
+  const initialStyles = window.getComputedStyle(element);
+  const initialPosition = initialStyles.position;
+  const containerStyles = window.getComputedStyle(container);
+  element.setAttribute(CONFIGS.addedFlagAttr, CONFIGS.addedFlagAttr);
+  if (isStatic(containerStyles.position)) {
+    container.style.position = 'relative';
+  }
+  _init();
+  const reset = () => {
+    element.style.position = initialPosition;
+    setStyles(element);
+    _init();
+  };
+
+  const destroy = (): void => {
+    if (removeHandle) {
+      removeHandle();
+      removeHandle = undefined;
+    }
+  };
+
+  return { destroy, reset };
 }
 
-export function initAllBySelector(
-  selector: string,
-  root = window.document,
-): void {
+export function initBySelector(selector: string, root = window.document) {
+  const controllers: Array<ReturnType<typeof initStickyElement>> = [];
   root.querySelectorAll<HTMLElement>(selector).forEach(element => {
     if (!element.hasAttribute(CONFIGS.addedFlagAttr)) {
-      initStickyElement(element);
+      controllers.push(initStickyElement(element));
     }
   });
+  const destroy = () => {
+    controllers.forEach(({ destroy }) => {
+      destroy();
+    });
+  };
+  const reset = () => {
+    controllers.forEach(({ reset }) => {
+      reset();
+    });
+  };
+  return { destroy, reset };
 }
+
+/**
+ * @deprecated
+ * @see initBySelector
+ */
+export const initAllBySelector = initBySelector;
