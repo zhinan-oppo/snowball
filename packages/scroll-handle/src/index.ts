48f1613e82c65de971539457af4528257e5c59cb
xsfour <xsfour@qq.com>
2020-05-24 21:13:22 +0800

    refactor(scroll-handle): pass startY to handlers

--- packages/scroll-handle/src/index.ts
+++ packages/scroll-handle/src/index.ts
@@ -15,6 +15,7 @@ type Handler = (
   dom: Element,
   distance: number,
   totalDistance: number,
+  startY: number,
 ) => void | 'done';
 
 export interface ScrollHandlers {
@@ -44,7 +45,7 @@ function getPercentFromAlias(alias: PercentToTop): number {
   return PERCENTS[alias] || 0;
 }
 
-export function scrollHandle(
+export function addScrollListener(
   element: Element,
   {
     handlers = {},
@@ -59,7 +60,15 @@ export function scrollHandle(
     addListener = true,
     container = window,
   }: ScrollHandleOptions,
-): () => void {
+) {
+  if (!(element instanceof window.HTMLElement)) {
+    const e = new TypeError(`HTMLElement needed`);
+    console.error(e);
+    return {
+      removeEventListener: () => undefined,
+      getRangeY: () => [],
+    };
+  }
   const start =
     typeof _start === 'string' || typeof _start === 'number'
       ? {
@@ -80,6 +89,15 @@ export function scrollHandle(
           percent: getPercentFromAlias(_end.percent || 'top'),
           distance: _end.distance || 0,
         };
+  const getRangeY = () => {
+    const containerHeight =
+      container === window
+        ? getWindowHeight()
+        : (container as Element).getBoundingClientRect().height;
+    const startY = start.percent * containerHeight + start.distance;
+    const endY = end.percent * containerHeight + end.distance;
+    return [startY, endY];
+  };
 
   let state: State = 'before';
   const changeState = (newState: State): void => {
@@ -92,42 +110,45 @@ export function scrollHandle(
   };
   // FIXME: container 大小位置相关的属性需要判断是否是 windows
   const handle = (): void => {
-    const containerHeight =
-      container === window
-        ? getWindowHeight()
-        : (container as Element).getBoundingClientRect().height;
     const domRect = element.getBoundingClientRect();
     const top = domRect.top;
     const bottom = domRect.bottom;
     const height = domRect.height;
-    const startY = start.percent * containerHeight + start.distance;
-    const endY = end.percent * containerHeight + end.distance;
+    const [startY, endY] = getRangeY();
     const distance = startY - top;
     const totalDistance = startY - endY + height;
     if (top > startY) {
       changeState('before');
       if (handlers.before) {
-        if (handlers.before(element, distance, totalDistance) === 'done') {
+        if (
+          handlers.before(element, distance, totalDistance, startY) === 'done'
+        ) {
           handlers.before = undefined;
         }
       }
     } else if (bottom >= endY) {
       changeState('inView');
       if (handlers.inView) {
-        if (handlers.inView(element, distance, totalDistance) === 'done') {
+        if (
+          handlers.inView(element, distance, totalDistance, startY) === 'done'
+        ) {
           handlers.inView = undefined;
         }
       }
     } else {
       changeState('after');
       if (handlers.after) {
-        if (handlers.after(element, distance, totalDistance) === 'done') {
+        if (
+          handlers.after(element, distance, totalDistance, startY) === 'done'
+        ) {
           handlers.after = undefined;
         }
       }
     }
     if (handlers.always) {
-      if (handlers.always(element, distance, totalDistance) === 'done') {
+      if (
+        handlers.always(element, distance, totalDistance, startY) === 'done'
+      ) {
         handlers.always = undefined;
       }
     }
@@ -136,7 +157,7 @@ export function scrollHandle(
   const handler = (): void => {
     window.requestAnimationFrame(handle);
   };
-  const removeHandle = addListener
+  const removeScrollHandler = addListener
     ? (): void => container.removeEventListener('scroll', handler)
     : () => undefined;
   if (addListener) {
@@ -147,7 +168,12 @@ export function scrollHandle(
   }
 
   window.setTimeout(() => handle(), 0);
-  return removeHandle;
+  return { removeScrollHandler, getRangeY };
+}
+
+export function scrollHandle(element: Element, options: ScrollHandleOptions) {
+  const { removeScrollHandler } = addScrollListener(element, options);
+  return removeScrollHandler;
 }
 
 export default scrollHandle;
