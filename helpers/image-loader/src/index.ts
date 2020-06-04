26015e58a6fa64b55d3cff016b6aab0aa4ef81ee
xsfour <xsfour@qq.com>
2020-06-04 12:13:40 +0800

    refactor(image-loader): can load cached files

--- helpers/image-loader/src/index.ts
+++ helpers/image-loader/src/index.ts
@@ -5,6 +5,8 @@ import { getOptions, interpolateName, parseQuery } from 'loader-utils';
 import validate from 'schema-utils';
 import sharp from 'sharp';
 import { compilation, loader, Logger } from 'webpack';
+import * as path from 'path';
+import * as stdFS from 'fs';
 
 import schema from './options.schema';
 
@@ -16,6 +18,8 @@ interface Options {
   quality?: number;
   qualityMin?: number;
   progressive?: boolean;
+  cacheDir?: string;
+  context?: string;
 }
 
 const LOADER_NAME = 'ImageLoader';
@@ -98,7 +102,7 @@ export default async function(
     throw new Error('async() failed');
   }
 
-  const options = (getOptions(this) as Options) || {};
+  const { cacheDir, context, ...options } = (getOptions(this) as Options) || {};
   const {
     ratios = options.ratios,
     name = options.name,
@@ -116,6 +120,7 @@ export default async function(
       name,
       type,
       esModule,
+      cacheDir,
     },
     {
       name: LOADER_NAME,
@@ -123,54 +128,103 @@ export default async function(
     },
   );
 
-  try {
-    const { format, images } = await resize(source, ratios, logger);
-    const plugins =
-      quality < 100 && ['png', 'jpeg'].includes(format)
-        ? format === 'png'
-          ? [png({ quality: [qualityMin / 100, quality / 100], strip: true })]
-          : [jpeg({ quality, progressive })]
-        : undefined;
-    const files = await Promise.all(
-      images.map(async (p) => {
-        const { content, width, ratio } = await p;
-        const filename = interpolateName(
-          this,
-          name
-            .replace('[width]', width)
-            .replace('[ratio]', ratio.toString().replace('0.', 'd')),
-          {
-            content,
-            context: this.rootContext,
-          },
+  let resFiles: any[] = [];
+  /**
+   * FIXME
+   */
+  if (cacheDir) {
+    const fs = (this.fs || stdFS) as typeof stdFS;
+    const filename = interpolateName(this, '[path]/[name].[ext]', {
+      context: context || this.rootContext,
+    });
+    const parsed = path.parse(filename);
+    const dir = path.resolve(cacheDir, parsed.dir);
+    const filenames = fs.readdirSync(dir);
+    resFiles = await Promise.all(
+      ratios.map(async (ratio: number) => {
+        const nameWithRatio = new RegExp(
+          `^${parsed.name}_\\d+@${ratio
+            .toString()
+            .replace('0.', 'd')}-.*${parsed.ext.replace('.', '')}$`,
         );
-        const resBuffer = !plugins
-          ? content
-          : await imagemin.buffer(content, { plugins });
-        return { filename, width, content: resBuffer };
+        for (const filename of filenames) {
+          if (nameWithRatio.test(filename)) {
+            logger.info(`matched: ${dir}/${filename}`);
+            const content = fs.readFileSync(path.resolve(dir, filename));
+            try {
+              const { width } = await sharp(content).metadata();
+              return {
+                width,
+                content,
+                filename: interpolateName(
+                  this,
+                  name
+                    .replace('[width]', width)
+                    .replace('[ratio]', ratio.toString().replace('0.', 'd')),
+                  {
+                    content,
+                    context: this.rootContext,
+                  },
+                ),
+              };
+            } catch (e) {
+              logger.error(e);
+            }
+          }
+        }
+        logger.warn(`not found: ${nameWithRatio}`);
       }),
     );
-    const res =
-      (esModule ? 'export default ' : 'module.exports = ') +
-      files
-        .map(({ filename, width }) => {
-          let code = `__webpack_public_path__ + ${JSON.stringify(filename)}`;
-          if (type === 'srcset') {
-            code += ` + ' ${width}w'`;
-          }
-          return code;
-        })
-        .join(' + ", " + ');
-
-    if (this.loaderIndex === 0) {
-      /** 没有使用缓存，直接 emitFile */
-      emitFiles(this, files);
-      return callback(undefined, res);
+  } else {
+    try {
+      const { format, images } = await resize(source, ratios, logger);
+      const plugins =
+        quality < 100 && ['png', 'jpeg'].includes(format)
+          ? format === 'png'
+            ? [png({ quality: [qualityMin / 100, quality / 100], strip: true })]
+            : [jpeg({ quality, progressive })]
+          : undefined;
+      resFiles = await Promise.all(
+        images.map(async (p) => {
+          const { content, width, ratio } = await p;
+          const filename = interpolateName(
+            this,
+            name
+              .replace('[width]', width)
+              .replace('[ratio]', ratio.toString().replace('0.', 'd')),
+            {
+              content,
+              context: this.rootContext,
+            },
+          );
+          const resBuffer = !plugins
+            ? content
+            : await imagemin.buffer(content, { plugins });
+          return { filename, width, content: resBuffer };
+        }),
+      );
+    } catch (e) {
+      logger.error(e);
+      return callback(e);
     }
-
-    callback(undefined, undefined, res, ...(files as any));
-  } catch (e) {
-    logger.error(e);
-    callback(e);
   }
+  const res =
+    (esModule ? 'export default ' : 'module.exports = ') +
+    resFiles
+      .map(({ filename, width }) => {
+        let code = `__webpack_public_path__ + ${JSON.stringify(filename)}`;
+        if (type === 'srcset') {
+          code += ` + ' ${width}w'`;
+        }
+        return code;
+      })
+      .join(' + ", " + ');
+
+  if (this.loaderIndex === 0) {
+    /** 没有使用缓存，直接 emitFile */
+    emitFiles(this, files);
+    return callback(undefined, res);
+  }
+
+  callback(undefined, undefined, res, ...(resFiles as any));
 }
