6b102adbfcc84aa8911e46bf896056123151fd6b
xsfour <xsfour@qq.com>
2020-08-03 12:09:24 +0800

    fix(scroll-handle): wrong capturing group order

--- packages/scroll-handle/src/placement.ts
+++ packages/scroll-handle/src/placement.ts
@@ -44,7 +44,8 @@ export function resolveCSSPlacement(str: string | number): ResolvedPlacement {
   }
   const matches = str.match(/(-?\d+(\.\d*)?)(%|px)?/i);
   if (matches) {
-    if (matches[2] === '%') {
+    console.log(matches);
+    if (matches[3] === '%') {
       return {
         percent: parseFloat(matches[1]) / 100,
         distance: 0,
